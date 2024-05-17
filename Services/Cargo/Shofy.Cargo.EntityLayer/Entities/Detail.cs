702b9d9f947ce3eef0cbed586fa8653a995a71d3
NihadSoltanov1 <mr.nihadsoltanov@gmail.com>
2024-05-17 23:27:37 +0400

    cargo service and n layer arch

---
+++ Services/Cargo/Shofy.Cargo.EntityLayer/Entities/Detail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shofy.Cargo.EntityLayer.Entities
+{
+    public class Detail
+    {
+        public int Id { get; set; }
+        public string SenderCustomer { get; set; }
+        public string ReceiverCustomer { get; set; }
+        public int Barcode { get; set; }
+        public Company Company { get; set; }
+    }
+}
