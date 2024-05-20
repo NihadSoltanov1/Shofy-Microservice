c89a2ba05a37ffa57c1675232420b9e9aada3624
NihadSoltanov1 <mr.nihadsoltanov@gmail.com>
2024-05-20 22:35:50 +0400

    basket services

---
+++ Services/Basket/Shofy.Basket/Services/BasketService.cs
@@ -0,0 +1,32 @@
+﻿using Shofy.Basket.Dtos;
+using Shofy.Basket.Settings;
+using System.Text.Json;
+
+namespace Shofy.Basket.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly RedisService _redisService;
+
+        public BasketService(RedisService redisService)
+        {
+            _redisService = redisService;
+        }
+
+        public async Task DeleteBasket(string userId)
+        {
+            var status = await _redisService.GetDb().KeyDeleteAsync(userId);
+        }
+
+        public async Task<BasketTotalDto> GetBasket(string userId)
+        {
+            var existBasket = await _redisService.GetDb().StringGetAsync(userId);
+            return JsonSerializer.Deserialize<BasketTotalDto>(existBasket);
+        }
+
+        public async Task SaveBasket(BasketTotalDto totalBasket)
+        {
+            await _redisService.GetDb().StringSetAsync(totalBasket.UserId, JsonSerializer.Serialize(totalBasket));
+        }
+    }
+}
---
+++ Services/Basket/Shofy.Basket/Services/IBasketService.cs
@@ -0,0 +1,11 @@
+﻿using Shofy.Basket.Dtos;
+
+namespace Shofy.Basket.Services
+{
+    public interface IBasketService
+    {
+        Task<BasketTotalDto> GetBasket(string userId);
+        Task SaveBasket(BasketTotalDto basket);
+        Task DeleteBasket(string userId);
+    }
+}
